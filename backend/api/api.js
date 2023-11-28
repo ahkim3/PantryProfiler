d9d46b753c85473127bd9c7503d2aefbe2aa1c5b
Andrew Kim <andrewhkim3@gmail.com>
2023-11-27 20:51:28 -0600

    Add POST request to query Items

--- backend/api/api.js
+++ backend/api/api.js
@@ -15,11 +15,30 @@ app.use((req, res, next) => {
     next();
 });
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
 // API endpoint to query the database
 app.get("/api/query", async (req, res) => {
     try {
         await mssql.connect(dbConfig);
         const result = await mssql.query("SELECT * FROM LOCATIONS");
+        res.json(result.recordset);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    } finally {
+        await mssql.close();
+    }
+});
+
+// Handle POST request to query the Items table. Needs location ID.
+app.post("/api/query/items", async (req, res) => {
+    try {
+        await mssql.connect(dbConfig);
+        const result = await mssql.query(
+            `SELECT * FROM ITEMS WHERE location_id = ${req.body.location_id}`
+        );
         res.json(result.recordset);
     } catch (err) {
         console.error(err);
