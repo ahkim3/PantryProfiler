362752edd3fd3f67ffdf68bec7deb7fe4c7e654c
Mackenna Hale <98496883+mmh9xx@users.noreply.github.com>
2023-11-29 13:16:08 -0600

    upload ui

---
+++ frontend/src/pages/PantryLowInStock.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NavBar from './NavBar';
+
+const PantryLowInStock = () => {
+  return (
+    <div>
+        <NavBar />
+      <h1>Pantry Low In Stock</h1>
+      
+    </div>
+  );
+};
+
+export default PantryLowInStock;
---
+++ frontend/src/pages/RequestEPack.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import NavBar from './NavBar';
+import './style/Update.css';
+
+const EPackUpdate = () => {
+        const [inputs, setInputs] = useState({
+          EPackLocation: '',
+          EPackQuantity: '',
+        });
+      
+        const handleChange = (e) => {
+          const { name, value } = e.target;
+          setInputs({ ...inputs, [name]: value });
+        };
+      
+        const handleSubmit = (e) => {
+          e.preventDefault();
+          // Do something with the form data, like sending it to a server
+          console.log('Form submitted:', inputs);
+        };
+      
+        return (
+        <div>
+        <NavBar />
+        <h1>Request Emergency Food Packs</h1>
+        <div class="dynamic-box">
+            <div class="form">
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="EPackLocation"></label>
+              <input
+                type="text"
+                id="EPackLocation"
+                name="EPackLocation"
+                value={inputs.EPackLocation}
+                onChange={handleChange}
+                placeholder="Location"
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="EPackQuantity"></label>
+              <input
+                type="text"
+                id="EPackQuantity"
+                name="EPackQuantity"
+                value={inputs.EPackQuantity}
+                onChange={handleChange}
+                placeholder="Quantity"
+                required
+              />
+            </div>
+            <button class="button-submit" type="submit">Update</button>
+          </form>
+          </div>
+          </div>
+          </div>
+        );
+      };
+
+export default EPackUpdate;
