ebf2fe9af5b0efb5e2c418ed0024055aac8f086f
Mackenna Hale <mmh9xx@umsystem.edu>
2023-12-01 13:32:23 -0600

    revert to non editable table

--- frontend/src/pages/EPackLocations.js
+++ frontend/src/pages/EPackLocations.js
@@ -20,42 +20,42 @@ const EPackLocations = () => {
 
     fetchData();
 
-     const [editableRowIndex, setEditableRowIndex] = useState(null);
-     const [quantityData, setQuantityData] = useState(responseData); // responseData is assumed to be your initial data
+    // const [editableRowIndex, setEditableRowIndex] = useState(null);
+    // const [quantityData, setQuantityData] = useState(responseData); // responseData is assumed to be your initial data
   
-     const handleEdit = (index) => {
-       setEditableRowIndex(index);
-     };
+    // const handleEdit = (index) => {
+    //   setEditableRowIndex(index);
+    // };
   
-     const handleQuantityChange = (event, index) => {
-       const newQuantityData = [...quantityData];
-       newQuantityData[index].quantity = event.target.value;
-       setQuantityData(newQuantityData);
-     };
+    // const handleQuantityChange = (event, index) => {
+    //   const newQuantityData = [...quantityData];
+    //   newQuantityData[index].quantity = event.target.value;
+    //   setQuantityData(newQuantityData);
+    // };
   
-     const handleSubmit = async () => {
-       try {
-         // Assuming your API endpoint is '/api/updateQuantity'
-         const response = await fetch('/api/items/update', {
-           method: 'PUT', // or 'POST' depending on your API
-           headers: {
-             'Content-Type': 'application/json',
-           },
-           body: JSON.stringify(quantityData),
-         });
+    // const handleSubmit = async () => {
+    //   try {
+    //     // Assuming your API endpoint is '/api/updateQuantity'
+    //     const response = await fetch('/api/items/update', {
+    //       method: 'PUT', // or 'POST' depending on your API
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify(quantityData),
+    //     });
   
-         if (response.ok) {
-           setEditableRowIndex(null); // Reset editableRowIndex after submitting changes
-           // You might want to handle success or show a success message here
-         } else {
-           // Handle errors or show an error message
-           console.error('Failed to update data');
-         }
-       } catch (error) {
-         // Handle network errors
-         console.error('Error:', error);
-       }
-     };
+    //     if (response.ok) {
+    //       setEditableRowIndex(null); // Reset editableRowIndex after submitting changes
+    //       // You might want to handle success or show a success message here
+    //     } else {
+    //       // Handle errors or show an error message
+    //       console.error('Failed to update data');
+    //     }
+    //   } catch (error) {
+    //     // Handle network errors
+    //     console.error('Error:', error);
+    //   }
+    // };
   
   }, []);
 
@@ -76,28 +76,12 @@ const EPackLocations = () => {
             {responseData &&
               responseData.map((dataItem, index) => (
                 <tr key={index}>
-                  <td>{dataItem.location_name}</td>
-                  <td>{editableRowIndex === index ? (
-                    <input
-                      type="number"
-                      value={dataItem.quantity}
-                      onChange={(event) => handleQuantityChange(event, index)}
-                    />
-                  ) : (
-                    dataItem.quantity
-                  )}</td>
-                  <td>
-                  {editableRowIndex !== index ? (
-                    <button onClick={() => handleEdit(index)}>Edit</button>
-                  ) : null}
-                </td>
+                  <td id={`location_name_${index}`}>{dataItem.location_name}</td>
+                  <td id={`quantity_${index}`}>{dataItem.quantity}</td>
                 </tr>
               ))}
           </tbody>
         </table>
-        {editableRowIndex !== null && (
-        <button onClick={handleSubmit}>Submit Changes</button>
-      )}
       </div>
     </div>
   );
