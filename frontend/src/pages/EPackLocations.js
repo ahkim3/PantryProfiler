60e3f0fe90bd36283d2703c4e4f2e05e63df137b
Mackenna Hale <mmh9xx@umsystem.edu>
2023-12-03 19:21:57 -0600

    button to update

--- frontend/src/pages/EPackLocations.js
+++ frontend/src/pages/EPackLocations.js
@@ -5,22 +5,65 @@ import './style/Table.css';
 
 const EPackLocations = () => {
   const [responseData, setResponseData] = useState(null);
-  
+  const [editedData, setEditedData] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const apiUrl = 'http://20.12.134.62:3000/api/epacks/query';
-        const response = await axios.post(apiUrl);
-        setResponseData(response.data);
-        console.log(response.data);
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
-
     fetchData();
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const apiUrl = 'http://20.12.134.62:3000/api/epacks/query';
+      const response = await axios.post(apiUrl);
+      const retrievedData = response.data.map(item => ({ ...item, isEditing: false }));
+      setResponseData(retrievedData);
+      setEditedData(retrievedData);
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleEdit = (index) => {
+    const updatedData = [...editedData];
+    updatedData[index].isEditing = true;
+    setEditedData(updatedData);
+  };
+
+  const handleQuantityChange = (event, index) => {
+    const updatedData = [...editedData];
+    updatedData[index].quantity = event.target.value;
+    setEditedData(updatedData);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      console.log('Submitting changes:', editedData);
+      await Promise.all(
+        editedData.map(async (dataItem) => {
+          if (dataItem.id) {
+            try {
+              const response = await axios.put(
+                `http://20.12.134.62:3000/api/epacks/update`,
+                {
+                  pack_id: dataItem.pack_id,
+                  location_id: dataItem.location_id,
+                  new_stock_level: dataItem.quantity // Use new_stock_level field for API
+                }
+              );
+              console.log('Updated data:', response.data);
+            } catch (error) {
+              console.error('Error updating data:', error);
+            }
+          }
+        })
+      );
+      fetchData(); 
+    } catch (error) {
+      console.error('Error submitting changes:', error);
+    }
+  };
+  
 
   return (
     <div>
@@ -28,28 +71,36 @@ const EPackLocations = () => {
       <h1>Emergency Pack Locations</h1>
 
       <div className="dynamic-box">
-      
-        <table className="table" >
-        <thead>
+        <table className="table">
+          <thead>
             <tr>
               <th>Location</th>
               <th>Quantity</th>
             </tr>
           </thead>
           <tbody>
-            {responseData &&
-              responseData.map((dataItem, index) => (
+            {editedData &&
+              editedData.map((dataItem, index) => (
                 <tr key={index}>
                   <td>{dataItem.location_name}</td>
-                  <td>{dataItem.quantity}</td>
+                  <td onClick={() => handleEdit(index)}>
+                    {dataItem.isEditing ? (
+                      <input
+                        type="number"
+                        value={dataItem.quantity}
+                        onChange={(event) => handleQuantityChange(event, index)}
+                      />
+                    ) : (
+                      dataItem.quantity
+                    )}
+                  </td>
                 </tr>
               ))}
           </tbody>
         </table>
+        <button className="button-submit" onClick={handleSubmit}>Submit Changes</button>
       </div>
     </div>
-        
-
   );
 };
 
